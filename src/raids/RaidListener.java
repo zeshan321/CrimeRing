7299795eecee01e93f495482c29e5b0471ebe7f2
Zeshan Aslam <zeshan12aslam@gmail.com>
2016-08-12 23:16:24 -0400

    Raid death listener

--- src/raids/RaidListener.java
+++ src/raids/RaidListener.java
@@ -1,11 +1,15 @@
 package raids;
 
+import com.zeshanaslam.crimering.FileHandler;
 import com.zeshanaslam.crimering.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
@@ -56,6 +60,26 @@ public class RaidListener implements Listener {
 
         if (event.getSlot() == 17) {
             player.closeInventory();
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDeathRaid(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+
+        PartyAPI partyAPI = new PartyAPI();
+        PartyObject party = partyAPI.getParty(player);
+
+        if (party == null) {
+            if (plugin.raidManager.raids.containsKey(player)) {
+                FileHandler fileHandler = new FileHandler("plugins/CrimeRing/raids/" + plugin.raidManager.raids.get(player) + ".yml");
+                player.teleport(new Location(Bukkit.getWorld(fileHandler.getString("info.world")), fileHandler.getInteger("info.x"), fileHandler.getInteger("info.y"), fileHandler.getInteger("info.z"), fileHandler.getInteger("info.yaw"), fileHandler.getInteger("info.pitch")));
+            }
+        } else {
+            if (plugin.raidManager.raids.containsKey(party.getOwner())) {
+                FileHandler fileHandler = new FileHandler("plugins/CrimeRing/raids/" + plugin.raidManager.raids.get(party.getOwner()) + ".yml");
+                player.teleport(new Location(Bukkit.getWorld(fileHandler.getString("info.world")), fileHandler.getInteger("info.x"), fileHandler.getInteger("info.y"), fileHandler.getInteger("info.z"), fileHandler.getInteger("info.yaw"), fileHandler.getInteger("info.pitch")));
+            }
         }
     }
 }
