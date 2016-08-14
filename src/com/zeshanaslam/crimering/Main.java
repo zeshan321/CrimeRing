cf102332251efbe7c9d68743b25ae15c505613e2
Zeshan Aslam <zeshan12aslam@gmail.com>
2016-08-14 19:07:32 -0400

    Hide potion particles

--- src/com/zeshanaslam/crimering/Main.java
+++ src/com/zeshanaslam/crimering/Main.java
@@ -7,15 +7,13 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.FieldAccessException;
+import com.comphenix.protocol.utility.MinecraftVersion;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import commands.Edit;
 import commands.Item;
 import commands.Reload;
 import events.BasicEvents;
-import net.minecraft.server.v1_10_R1.NBTTagCompound;
-import net.minecraft.server.v1_10_R1.NBTTagList;
-import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_10_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,9 +23,9 @@ import raids.RaidListener;
 import raids.RaidManager;
 import raids.RaidSetup;
 import script.*;
+import utils.ProtocolUtil;
 
 import java.io.File;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -42,27 +40,6 @@ public class Main extends JavaPlugin {
     public WorldGuardPlugin worldGuardPlugin;
     public ArrayList<String> flag = new ArrayList<>();
     public HashMap<String, Integer> values = new HashMap<>();
-
-    public static net.minecraft.server.v1_10_R1.ItemStack removeAttributes(net.minecraft.server.v1_10_R1.ItemStack i) {
-        if (i == null) {
-            return i;
-        }
-        if (net.minecraft.server.v1_10_R1.Item.getId(i.getItem()) == 386) {
-            return i;
-        }
-        net.minecraft.server.v1_10_R1.ItemStack item = i.cloneItemStack();
-        NBTTagCompound tag;
-        if (!item.hasTag()) {
-            tag = new NBTTagCompound();
-            item.setTag(tag);
-        } else {
-            tag = item.getTag();
-        }
-        NBTTagList am = new NBTTagList();
-        tag.set("AttributeModifiers", am);
-        item.setTag(tag);
-        return item;
-    }
 
     public void onEnable() {
         saveDefaultConfig();
@@ -128,6 +105,8 @@ public class Main extends JavaPlugin {
         getCommand("CREdit").setExecutor(new Edit(this));
         getCommand("CRItem").setExecutor(new Item(this));
 
+        ProtocolUtil protocolUtil = new ProtocolUtil();
+
         // ProtocolLib remove attributes
         Set<PacketType> packets = new HashSet<>();
         packets.add(PacketType.Play.Server.SET_SLOT);
@@ -143,7 +122,7 @@ public class Main extends JavaPlugin {
                     try {
                         ItemStack[] read = packet.getItemArrayModifier().read(0);
                         for (int i = 0; i < read.length; i++) {
-                            read[i] = removeAttributes(read[i]);
+                            read[i] = protocolUtil.removeAttributes(read[i]);
                         }
                         packet.getItemArrayModifier().write(0, read);
                     } catch (Exception e) {
@@ -157,7 +136,7 @@ public class Main extends JavaPlugin {
                     // Fix support
                 } else {
                     try {
-                        packet.getItemModifier().write(0, removeAttributes(packet.getItemModifier().read(0)));
+                        packet.getItemModifier().write(0, protocolUtil.removeAttributes(packet.getItemModifier().read(0)));
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -166,6 +145,7 @@ public class Main extends JavaPlugin {
 
         });
 
+        // Don't allow opening inventory while scoped in.
         ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this,
                 ListenerPriority.NORMAL,
                 PacketType.Play.Client.CLIENT_COMMAND) {
@@ -181,6 +161,7 @@ public class Main extends JavaPlugin {
             }
         });
 
+        // Cancel scope when effect runs out
         ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this,
                 ListenerPriority.NORMAL,
                 PacketType.Play.Server.REMOVE_ENTITY_EFFECT) {
@@ -193,87 +174,23 @@ public class Main extends JavaPlugin {
                 }
             }
         });
+
+        // Hide potion particles
+        final int POTION_INDEX = ProtocolLibrary.getProtocolManager().getMinecraftVersion().compareTo(new MinecraftVersion("1.10.2")) <= 0 ? 8 : 7;
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this,
+                PacketType.Play.Server.ENTITY_METADATA) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                Entity entity = event.getPacket().getEntityModifier(event).read(0);
+
+                if (event.getPlayer().equals(entity)) {
+                    protocolUtil.modifyWatchable(event, POTION_INDEX, 0);
+                }
+            }
+        });
     }
 
     public void onDisable() {
         saveConfig();
-    }
-
-    private ItemStack removeAttributes(ItemStack i) {
-        if (i == null) {
-            return i;
-        }
-
-        if (i.getType() == Material.BOOK_AND_QUILL) {
-            return i;
-        }
-
-        ItemStack item = i.clone();
-        net.minecraft.server.v1_10_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
-
-        NBTTagCompound tag;
-        if (!nmsStack.hasTag()) {
-            tag = new NBTTagCompound();
-            nmsStack.setTag(tag);
-        } else {
-            tag = nmsStack.getTag();
-        }
-
-        NBTTagList am = new NBTTagList();
-        tag.set("AttributeModifiers", am);
-        nmsStack.setTag(tag);
-
-        return setUnbreakable(CraftItemStack.asCraftMirror(nmsStack));
-    }
-
-    private ItemStack setUnbreakable(ItemStack i) {
-        if (!(i instanceof CraftItemStack)) {
-            i = CraftItemStack.asCraftCopy(i);
-        }
-        NBTTagCompound tag = getTag(i);
-        if (tag == null) {
-            tag = new NBTTagCompound();
-        }
-        tag.setInt("Unbreakable", 1);
-        return setTag(i, tag);
-    }
-
-    private NBTTagCompound getTag(org.bukkit.inventory.ItemStack item) {
-        if ((item instanceof CraftItemStack)) {
-            try {
-                Field field = CraftItemStack.class.getDeclaredField("handle");
-                field.setAccessible(true);
-                return ((net.minecraft.server.v1_10_R1.ItemStack) field.get(item)).getTag();
-            } catch (Exception e) {
-            }
-        }
-        return null;
-    }
-
-    private org.bukkit.inventory.ItemStack setTag(org.bukkit.inventory.ItemStack item, NBTTagCompound tag) {
-        CraftItemStack craftItem = null;
-        if ((item instanceof CraftItemStack)) {
-            craftItem = (CraftItemStack) item;
-        } else {
-            craftItem = CraftItemStack.asCraftCopy(item);
-        }
-        net.minecraft.server.v1_10_R1.ItemStack nmsItem = null;
-        try {
-            Field field = CraftItemStack.class.getDeclaredField("handle");
-            field.setAccessible(true);
-            nmsItem = (net.minecraft.server.v1_10_R1.ItemStack) field.get(item);
-        } catch (Exception e) {
-        }
-        if (nmsItem == null) {
-            nmsItem = CraftItemStack.asNMSCopy(craftItem);
-        }
-        nmsItem.setTag(tag);
-        try {
-            Field field = CraftItemStack.class.getDeclaredField("handle");
-            field.setAccessible(true);
-            field.set(craftItem, nmsItem);
-        } catch (Exception e) {
-        }
-        return craftItem;
     }
 }
---
+++ src/utils/ProtocolUtil.java
@@ -0,0 +1,120 @@
+package utils;
+
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import net.minecraft.server.v1_10_R1.NBTTagCompound;
+import net.minecraft.server.v1_10_R1.NBTTagList;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_10_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class ProtocolUtil {
+
+    public void modifyWatchable(PacketEvent event, int index, Object value) {
+        // Determine if we need to modify this packet
+        if (hasIndex(getWatchable(event), index)) {
+            // We do - clone it first, as it might have been broadcasted
+            event.setPacket(event.getPacket().deepClone());
+
+            for (WrappedWatchableObject object : getWatchable(event)) {
+                if (object.getIndex() == index) {
+                    object.setValue(value);
+                }
+            }
+        }
+    }
+
+    public List<WrappedWatchableObject> getWatchable(PacketEvent event) {
+        return event.getPacket().getWatchableCollectionModifier().read(0);
+    }
+
+    public boolean hasIndex(List<WrappedWatchableObject> list, int index) {
+        for (WrappedWatchableObject object : list) {
+            if (object.getIndex() == index) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ItemStack removeAttributes(ItemStack i) {
+        if (i == null) {
+            return i;
+        }
+
+        if (i.getType() == Material.BOOK_AND_QUILL) {
+            return i;
+        }
+
+        ItemStack item = i.clone();
+        net.minecraft.server.v1_10_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+
+        NBTTagCompound tag;
+        if (!nmsStack.hasTag()) {
+            tag = new NBTTagCompound();
+            nmsStack.setTag(tag);
+        } else {
+            tag = nmsStack.getTag();
+        }
+
+        NBTTagList am = new NBTTagList();
+        tag.set("AttributeModifiers", am);
+        nmsStack.setTag(tag);
+
+        return setUnbreakable(CraftItemStack.asCraftMirror(nmsStack));
+    }
+
+    public ItemStack setUnbreakable(ItemStack i) {
+        if (!(i instanceof CraftItemStack)) {
+            i = CraftItemStack.asCraftCopy(i);
+        }
+        NBTTagCompound tag = getTag(i);
+        if (tag == null) {
+            tag = new NBTTagCompound();
+        }
+        tag.setInt("Unbreakable", 1);
+        return setTag(i, tag);
+    }
+
+    public NBTTagCompound getTag(org.bukkit.inventory.ItemStack item) {
+        if ((item instanceof CraftItemStack)) {
+            try {
+                Field field = CraftItemStack.class.getDeclaredField("handle");
+                field.setAccessible(true);
+                return ((net.minecraft.server.v1_10_R1.ItemStack) field.get(item)).getTag();
+            } catch (Exception e) {
+            }
+        }
+        return null;
+    }
+
+    public org.bukkit.inventory.ItemStack setTag(org.bukkit.inventory.ItemStack item, NBTTagCompound tag) {
+        CraftItemStack craftItem = null;
+        if ((item instanceof CraftItemStack)) {
+            craftItem = (CraftItemStack) item;
+        } else {
+            craftItem = CraftItemStack.asCraftCopy(item);
+        }
+        net.minecraft.server.v1_10_R1.ItemStack nmsItem = null;
+        try {
+            Field field = CraftItemStack.class.getDeclaredField("handle");
+            field.setAccessible(true);
+            nmsItem = (net.minecraft.server.v1_10_R1.ItemStack) field.get(item);
+        } catch (Exception e) {
+        }
+        if (nmsItem == null) {
+            nmsItem = CraftItemStack.asNMSCopy(craftItem);
+        }
+        nmsItem.setTag(tag);
+        try {
+            Field field = CraftItemStack.class.getDeclaredField("handle");
+            field.setAccessible(true);
+            field.set(craftItem, nmsItem);
+        } catch (Exception e) {
+        }
+        return craftItem;
+    }
+}
